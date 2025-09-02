38b317d871bb5af64fed4d2e5279c1320f2c89bd
Tantawy03 <116743569+Tantawy03@users.noreply.github.com>
2025-09-02 12:16:27 +0300

    changed the colors

--- Subtask 2.py
+++ Subtask 2.py
@@ -15,9 +15,13 @@ red_mask = cv2.inRange(hsv_image, np.array([0, 50, 50]), np.array([10, 255, 255]
 black_mask = cv2.inRange(hsv_image, np.array([0, 0, 0]), np.array([180, 255, 30]))
 
 # Change all pixels that fit within the blue mask to black
+out[blue_mask > 0] = [0, 0, 0]
 # Change all pixels that fit within the red mask to blue
+out[red_mask > 0] = [255, 0, 0]
 # Change all pixels that fit within the black mask to red
+out[black_mask > 0] = [0, 0, 255]
 
+# Display the original and processed images side by side
 fig, axes = plt.subplots(1, 2)
 axes[0].imshow(img)
 axes[0].set_title('Original Image')
